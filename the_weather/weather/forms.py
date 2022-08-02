ec54b86e1cc5bac93d4fd78d103ff054dd8b9c6d
dracangelo <kelvinmbuguaw@gmail.com>
2022-08-02 11:47:46 +0300

    added a search functionality

---
+++ the_weather/weather/forms.py
@@ -0,0 +1,11 @@
+from dataclasses import fields
+from pyexpat import model
+from tkinter import Widget
+from django.forms import ModelForm, TextInput
+from .models import City
+
+class CityForm(ModelForm):
+    class Meta:
+        model=City
+        fields=['name']
+        widgets = {'name': TextInput(attrs={'class': 'input','placeholder': 'City name'})}
--- the_weather/weather/views.py
+++ the_weather/weather/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 import requests
+from .models import City
+from .forms import CityForm
 
 # Create your views here.
 
@@ -7,15 +9,27 @@ def index(request):
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=83c15dfafdd652afa05c27459ed7b1f1'
     city = 'Nairobi'
 
-    r = requests.get(url.format(city)).json()
+    if request.method == 'POST':
+        pass
 
-    city_weather = {
-        'city':city ,
-        'temparature':r['main']['temp'] ,
-        'description': r['weather'][0]['description'],  
-        'icon': r['weather'][0]['icon'],
-    }
+    form = CityForm()
 
-    context = {'city_weather':city_weather}
+    cities = City.objects.all()
+    weather_data = []
+
+    for city in cities:
+
+        r = requests.get(url.format(city)).json()
+
+        city_weather = {
+            'city':city.name ,
+            'temparature':r['main']['temp'] ,
+            'description': r['weather'][0]['description'],  
+            'icon': r['weather'][0]['icon'],
+        }
+
+        weather_data.append(city_weather)
+
+    context = {'weather_data':weather_data, 'form':form}
 
     return render(request, 'weather/weather.html', context)
