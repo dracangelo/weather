e854fc03f9e18218ddd7a3be0319a8ee656df161
dracangelo <kelvinmbuguaw@gmail.com>
2022-08-02 11:16:06 +0300

    created a city db

--- the_weather/weather/models.py
+++ the_weather/weather/models.py
@@ -1,3 +1,14 @@
+from tabnanny import verbose
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
+
+class City(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+        class Meta:
+            verbose_name_plural = 'cities'
